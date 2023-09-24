feaf244c67819bc6d515f29aa53674b7297933ab
MallikAdi <101927331+MallikAdi@users.noreply.github.com>
2023-09-24 18:30:06 +0530

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,10 +4,11 @@ import {
   Text,
   StyleSheet,
   TouchableOpacity,
-  ImageBackground,
-  Button,
   ActivityIndicator,
-  SafeAreaView
+  SafeAreaView,
+  Image,
+  Dimensions,
+  ImageBackground
 } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
@@ -44,9 +45,13 @@ const HomeScreen = ({ navigation, route }) => {
   const soilMoistureData = []
   const timeData = []
   const devId = []
+  const location = []
   // Loop through the jsonData array and extract and store data in respective arrays
   if (hasDevId) {
-    data.forEach((item) => devId.push(item.DevId))
+    /* data.forEach((item) => {
+      devId.push(item.DevId)
+      location.push(item.location)
+    }) */
   } else {
     data.forEach((item) => {
       batteryData.push(item.Batt)
@@ -60,101 +65,119 @@ const HomeScreen = ({ navigation, route }) => {
   console.log(temperatureData)
   console.log(humidityData)
   console.log(devId)
+  console.log(location)
   const image = { uri: '' }
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.multiButton}>
       {isLoading ? (
         <ActivityIndicator />
       ) : hasDevId ? (
-        <View>
+        <View style={styles.IdOptionsContainer}>
+          <View style={styles.imageContainer}>
+            <Image
+              style={styles.icon}
+              source={require('../../assets/HomePage.gif')}
+            />
+          </View>
           {data.map((item, index) => (
-            <Button
-              style={styles.multiButton}
-              key={index}
-              title={`DevId: ${item.DevId}`}
+            <TouchableOpacity
+              style={styles.squircleButton}
               onPress={() =>
                 navigation.navigate('MultiUser', {
                   name: 'MultiUser',
                   DevId: item.DevId
                 })
               }
-            />
+            >
+              <Text style={styles.buttonText}>
+                ID: {item.DevId} {'\n'}
+                Loc: {item.location}
+              </Text>
+            </TouchableOpacity>
           ))}
         </View>
       ) : (
         <View style={styles.container}>
-          <TouchableOpacity
-            style={[styles.button, styles.tempButton]}
-            onPress={() =>
-              navigation.navigate('Temp', {
-                name: 'Temp',
-                temperatureData: temperatureData
-              })
-            }
+          <ImageBackground
+            style={styles.battery}
+            source={require('../../assets/battery/battery-full.png')}
           >
-            <View style={styles.iconContainer}>
-              <Feather name={'thermometer'} size={24} color="white" />
-            </View>
-            <View style={styles.textContainer}>
-              <Text style={styles.title}>Temp</Text>
-              <Text style={styles.value}>{temperatureData[0]}</Text>
-            </View>
-          </TouchableOpacity>
+            <Text style={styles.batteryText}>{batteryData[0]}%</Text>
+          </ImageBackground>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={[styles.button, styles.tempButton]}
+              onPress={() =>
+                navigation.navigate('Temp', {
+                  name: 'Temp',
+                  temperatureData: temperatureData
+                })
+              }
+            >
+              <View style={styles.iconContainer}>
+                <Feather name={'thermometer'} size={24} color="white" />
+              </View>
+              <View style={styles.textContainer}>
+                <Text style={styles.title}>Temp</Text>
+                <Text style={styles.value}>{temperatureData[0]}</Text>
+              </View>
+            </TouchableOpacity>
 
-          <TouchableOpacity
-            style={[styles.button, styles.rainButton]}
-            onPress={() =>
-              navigation.navigate('Rain', {
-                name: 'Rain',
-                rainData: rainData
-              })
-            }
-          >
-            <View style={styles.iconContainer}>
-              <Feather name={'cloud-rain'} size={24} color="white" />
-            </View>
-            <View style={styles.textContainer}>
-              <Text style={styles.title}>Rain</Text>
-              <Text style={styles.value}>{rainData[0]}</Text>
-            </View>
-          </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.button, styles.rainButton]}
+              onPress={() =>
+                navigation.navigate('Rain', {
+                  name: 'Rain',
+                  rainData: rainData
+                })
+              }
+            >
+              <View style={styles.iconContainer}>
+                <Feather name={'cloud-rain'} size={24} color="white" />
+              </View>
+              <View style={styles.textContainer}>
+                <Text style={styles.title}>Rain</Text>
+                <Text style={styles.value}>{rainData[0]}</Text>
+              </View>
+            </TouchableOpacity>
 
-          <TouchableOpacity
-            style={[styles.button, styles.humButton]}
-            onPress={() =>
-              navigation.navigate('Humidity', {
-                name: 'Humidity',
-                humidityData: humidityData
-              })
-            }
-          >
-            <View style={styles.iconContainer}>
-              <Feather name={'droplet'} size={24} color="white" />
-            </View>
-            <View style={styles.textContainer}>
-              <Text style={styles.title}>Humidity</Text>
-              <Text style={styles.value}>{humidityData[0]}</Text>
-            </View>
-          </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.button, styles.humButton]}
+              onPress={() =>
+                navigation.navigate('Humidity', {
+                  name: 'Humidity',
+                  humidityData: humidityData
+                })
+              }
+            >
+              <View style={styles.iconContainer}>
+                <Feather name={'droplet'} size={24} color="white" />
+              </View>
+              <View style={styles.textContainer}>
+                <Text style={styles.title}>Humidity</Text>
+                <Text style={styles.value}>{humidityData[0]}</Text>
+              </View>
+            </TouchableOpacity>
 
-          <TouchableOpacity
-            style={[styles.button, styles.soilMButton]}
-            onPress={() =>
-              navigation.navigate('Soil Moisture', {
-                name: 'Soil Moisture',
-                soilMoistureData: soilMoistureData
-              })
-            }
-          >
-            <View style={styles.iconContainer}>
-              <Feather name={'align-center'} size={24} color="white" />
-            </View>
-            <View style={styles.textContainer}>
-              <Text style={styles.title}>Soil</Text>
-              <Text style={styles.title}>Moisture</Text>
-              <Text style={styles.value}>{soilMoistureData[0]}</Text>
-            </View>
-          </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.button, styles.soilMButton]}
+              onPress={() =>
+                navigation.navigate('Soil Moisture', {
+                  name: 'Soil Moisture',
+                  soilMoistureData: soilMoistureData
+                })
+              }
+            >
+              <View style={styles.iconContainer}>
+                <Feather name={'align-center'} size={24} color="white" />
+              </View>
+              <View style={styles.textContainer}>
+                <Text style={styles.title}>Soil</Text>
+                <Text style={styles.title}>Moisture</Text>
+                <Text style={styles.value}>{soilMoistureData[0]}</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
       )}
     </SafeAreaView>
@@ -164,16 +187,63 @@ const HomeScreen = ({ navigation, route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'stretch',
+    padding: 1
+  },
+  batteryContainer: {
+    alignSelf: 'flex-end',
+    backgroundColor: 'white'
+  },
+  battery: {
+    height: 541 / 11,
+    width: 541 / 11,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  batteryText: {
+    fontSize: 541 / 45,
+    color: 'white',
+    alignContent: 'center'
+  },
+  buttonContainer: {
+    flex: 1,
     flexDirection: 'row', // Horizontal arrangement
     justifyContent: 'center', // Center items horizontally
     alignItems: 'center', // Center items vertically
-    marginTop: 350,
-    backgroundColor: '#757b85'
+    //backgroundColor: '#f0f1f7'
+    backgroundColor: 'white'
   },
   imageLayout: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  IdOptionsContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start'
+  },
+  imageContainer: {
+    alignSelf: 'center'
+  },
+  icon: {
+    height: 640 / 3.5,
+    width: 640 / 3.5,
+    marginTop: 20,
+
+    marginBottom: Dimensions.get('window').height / 16
+    //padding: 50
+  },
+  squircleButton: {
+    //backgroundColor: 'white',
+    backgroundColor: '#262629',
+    //backgroundColor: '#383c40',
+    borderRadius: 20, // Adjust the border radius as needed
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    margin: 10
   },
   button: {
     height: 80,
@@ -184,6 +254,10 @@ const styles = StyleSheet.create({
     alignItems: 'center', // Center text horizontally
     shadowColor: 'black',
     padding: 10
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center'
   },
   iconContainer: {
     marginRight: 10,
@@ -215,7 +289,8 @@ const styles = StyleSheet.create({
   },
   multiButton: {
     flex: 1,
-    backgroundColor: '#69707a'
+    backgroundColor: 'white',
+    justifyContent: 'space-around'
   }
 })
 export default HomeScreen
