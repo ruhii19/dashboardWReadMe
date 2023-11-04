acd862e3e7cf47d98d5a81f8c74de48d99504a41
ruhii19 <101913978+ruhii19@users.noreply.github.com>
2023-11-04 15:50:22 +0530

    Update TempScreen.js

--- src/screens/parameters/TempScreen.js
+++ src/screens/parameters/TempScreen.js
@@ -4,10 +4,10 @@ import { LineChart } from 'react-native-chart-kit'
 import { Dimensions } from 'react-native'
 
 const TempScreen = ({ navigation, route }) => {
-  const { temperatureData } = route.params
+  const { temperatureData, timeData } = route.params
   const reversedTemp = temperatureData.slice(0, 10).reverse() // Reversed copy of the data
-  //const reversedTemp = temperatureData
-  // Calculate the sum of all values in the reversedSoil array
+
+  // Calculate the sum of all values in the reversedTemp array
   const sum = reversedTemp.reduce((acc, currentValue) => acc + currentValue, 0)
 
   // Calculate the average
@@ -15,14 +15,43 @@ const TempScreen = ({ navigation, route }) => {
 
   console.log('Temp data:', temperatureData)
   console.log('Average:', average)
+  console.log(timeData)
 
+  function extractTimeFromTimestamp(timestamp) {
+    const parts = timestamp.split(' ')
+    if (parts.length >= 4) {
+      const timeParts = parts[3].split(':')
+      if (timeParts.length >= 2) {
+        return `${timeParts[0]}:${timeParts[1]}`
+      }
+    }
+    return 'Invalid Timestamp'
+  }
+
+  function extractDateFromTimestamp(timestamp) {
+    const parts = timestamp.split(' ')
+    if (parts.length >= 3) {
+      return `${parts[0]} ${parts[1]}`
+    }
+    return 'Invalid Timestamp' // Handle the case where the timestamp format is not as expected
+  }
   const data = {
-    labels: [],
+    labels: [
+      [extractTimeFromTimestamp(timeData[9])],
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      [extractTimeFromTimestamp(timeData[0])]
+    ],
     datasets: [
       {
         data: reversedTemp,
-        color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`, // Line color for the first dataset
-        strokeWidth: 2 // Line width for the first dataset
+        color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`,
+        strokeWidth: 2
       }
     ]
   }
@@ -31,14 +60,14 @@ const TempScreen = ({ navigation, route }) => {
     <View style={styles.container}>
       <Image
         style={styles.icon}
-        source={require('../../../assets/watering-can.gif')}
+        source={require('../../../assets/temperature.gif')}
       />
       <View style={styles.textContainer}>
         <View style={styles.infoContainer}>
           <Text style={styles.par}>Current</Text>
           <Text style={styles.curr}>
             {temperatureData[0]}
-            <Text style={styles.unit}>%</Text>
+            <Text style={styles.unit}>°C</Text>
           </Text>
         </View>
 
@@ -46,7 +75,7 @@ const TempScreen = ({ navigation, route }) => {
           <Text style={styles.par}>Average</Text>
           <Text style={styles.curr}>
             {average.toFixed(1)}
-            <Text style={styles.unit}>%</Text>
+            <Text style={styles.unit}>°C</Text>
           </Text>
         </View>
       </View>
@@ -55,13 +84,13 @@ const TempScreen = ({ navigation, route }) => {
         data={data}
         width={Dimensions.get('window').width / 1.05}
         height={Dimensions.get('window').height / 3}
-        yAxisSuffix=" %"
+        yAxisSuffix="°C"
         yAxisInterval={1}
         chartConfig={{
           backgroundGradientFrom: 'white',
           backgroundGradientTo: 'white',
           decimalPlaces: 1,
-          color: (opacity = 1) => `rgba(	51, 204, 204, ${opacity})`,
+          color: (opacity = 1) => `rgba(51, 204, 204, ${opacity})`,
           labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
           style: {
             borderRadius: 16
@@ -77,7 +106,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    //backgroundColor: '#f0f1f7',
     backgroundColor: 'white',
     flexDirection: 'column',
     justifyContent: 'flex-start'
@@ -85,10 +113,7 @@ const styles = StyleSheet.create({
   icon: {
     height: 640 / 3.5,
     width: 640 / 3.5,
-    //marginTop: 20
-
     marginBottom: Dimensions.get('window').height / 16
-    //padding: 50
   },
   textContainer: {
     flexDirection: 'row',
@@ -104,9 +129,7 @@ const styles = StyleSheet.create({
   },
   curr: {
     fontSize: Dimensions.get('window').height / 16,
-    //fontFamily: 'sans-serif-thin',
     fontFamily: 'sans-serif-light',
-    //fontWeight: '100',
     marginBottom: Dimensions.get('window').height / 9
   },
   unit: {
